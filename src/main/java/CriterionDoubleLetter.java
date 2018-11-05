e6b8c8de52b2bad9ef486ba18294d96b15300363
Elias Norrby <elias.norrby@gmail.com>
2018-11-05 16:45:15 +0100

    Implement and test CriterionDoubleLetter

--- src/main/java/CriterionDoubleLetter.java
+++ src/main/java/CriterionDoubleLetter.java
@@ -1,6 +1,19 @@
 public class CriterionDoubleLetter implements ICriterion {
     @Override
     public boolean check(String string) {
-        return false;
+        String[] chars = string.split("");
+
+        String lastChar = chars[0];
+
+        boolean foundDoubleLetter = false;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i].equals(lastChar)) {
+                foundDoubleLetter = true;
+            }
+            lastChar = chars[i];
+        }
+
+        return foundDoubleLetter;
     }
 }
--- src/main/java/CriterionVowel.java
+++ src/main/java/CriterionVowel.java
@@ -3,13 +3,11 @@ import java.util.Arrays;
 
 public class CriterionVowel implements ICriterion {
 
-//    private char[] vowels = {'a', 'e', 'i', 'o', 'u'};
     private String[] vowels = {"a", "e", "i", "o", "u"};
 
     @Override
     public boolean check(String string) {
         int vowelCount = 0;
-//        char[] chars = string.toCharArray();
         String[] chars = string.split("");
 
         for (String c : chars) {
